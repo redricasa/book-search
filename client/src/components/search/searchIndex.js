5a18a05515624948afc885ac1153c06ae080d59f
redricasa <fredeb26@gmail.com>
2019-12-19 20:54:29 -0800

    added js and css cdn for materialize css in index.html. - added mongodb link

---
+++ client/src/components/search/searchIndex.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Search(props){
+    return(
+        <nav>
+            <div className="nav-wrapper">
+                <form>
+                    <div className="input-field">
+                    <input id="search" type="search" required>
+                    <label className="label-icon" for="search"><i className="material-icons">search</i></label>
+                    <i className="material-icons">close</i>
+                    </div>
+                </form>
+            </div>
+        </nav>
+    )
+};
+export default Search;
