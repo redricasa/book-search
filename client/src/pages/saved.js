ee921f4aeaeff9b877a87af288729e12d066b103
redricasa <fredeb26@gmail.com>
2019-12-27 21:11:14 -0800

    created an array to push cards into by looping through API results

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -3,70 +3,72 @@ import Card from "../components/Cards/saved.cards.index";
 import Navbar from "../components/navbar/navbar.index"
 import Header from "../components/header/header.index";
 import API from "../utils/API";
-
+import Book from "../components/Book/book.index";
 
 class Saved extends Component {
-  state = {
-    books: []
-  };
+    state = {
+        books: []
+    };
+    // bind this
+    constructor(props) {
+        super(props);
+        // This binding is necessary to make `this` work in the callback
+        this.getSavedBooks = this.getSavedBooks.bind(this);
+    };
+    componentDidMount() {
+        this.getSavedBooks();
+    }
 
-  componentDidMount() {
-    this.getSavedBooks();
-  }
+    getSavedBooks = () => {
+        API.getSavedBooks()
+        .then(res =>
+            this.setState({
+            books: res.data
+            })
+        )
+        .catch(err => console.log(err));
+    };
 
-  getSavedBooks = () => {
-    API.getSavedBooks()
-      .then(res =>
-        this.setState({
-          books: res.data
-        })
-      )
-      .catch(err => console.log(err));
-  };
-
-  handleBookDelete = id => {
-    API.deleteBook(id).then(res => this.getSavedBooks());
-  };
-    //TODO stretch goal: add handler to view book and get directed to amazon page to buy book
+    handleBookDelete = id => {
+        API.deleteBook(id).then(res => this.getSavedBooks());
+    };
+        //TODO stretch goal: add handler to view book and get directed to amazon page to buy book
 
 
 
     render() {
         // if there's results, display; if not display "no saved books"
-        this.state.books.length 
-        ? 
-        
-        this.state.books.map
-        (book => 
-            (
-            <Book
-                key={book._id}
-                title={book.title}
-                link={book.link}
-                authors={book.authors.join(", ")}
-                description={book.description}
-                image={book.image}
-                Button={() => (
-                    <a className="btn-floating btn-large waves-effect waves-light red">
-                        onClick={() => this.handleBookDelete(book._id)}<i className="material-icons">delete</i>
-                    </a>
-                )}
-            />
+        // TODO push cards into an array 
+        var Cards = [];
+        // loops through the geezNums.json and renders images
+        for(let index=0;index< API.length ;index++){
+            Cards
+            .push(
+                // calls the geez function in geez/index.js
+                <Book
+                    key={API[index]._id}
+                    title={API[index].title}
+                    link={API[index].link}
+                    authors={API[index].authors.join(", ")}
+                    description={API[index].description}
+                    image={API[index].image}
+                    Button={() => (
+                        <a className="btn-floating btn-large waves-effect waves-light red">
+                            onClick={() => this.handleBookDelete(API[index]._id)}<i className="material-icons">delete</i>
+                        </a>
+                    )}
+                />
             )
-        )
-        : 
-        (
-            <React.Fragment>
-                <h2 className="text-center">No Saved Books</h2>
-            </React.Fragment>
-        )
-        
+        }
         return (
             <React.Fragment>
                 <Navbar/>
                 <Header/>
+            
+                {Cards}
                 
             </React.Fragment>
+
         )
         
     }
