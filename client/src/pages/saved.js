5cace63c45ec8ff8893060a47aa8b918efa8895d
redricasa <fredeb26@gmail.com>
2019-12-27 20:32:45 -0800

    formatted saved.js to get rid of red squigglies. squiggliesgit add .

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -33,29 +33,34 @@ class Saved extends Component {
 
     render() {
         // if there's results, display; if not display "no saved books"
-        {
-            (this.state.books.length )
-            ? 
+        this.state.books.length 
+        ? 
+        
+        this.state.books.map
+        (book => 
             (
-                // {this.state.books.map(book => (
-                <Book
-                    key={book._id}
-                    title={book.title}
-                    link={book.link}
-                    authors={book.authors.join(", ")}
-                    description={book.description}
-                    image={book.image}
-                    Button={() => (
-                        <a className="btn-floating btn-large waves-effect waves-light red">
-                            onClick={() => this.handleBookDelete(book._id)}<i className="material-icons">delete</i>
-                        </a>
-                    )}
-                />
-                // )
-            ): (
-                <h2 className="text-center">No Saved Books</h2>
+            <Book
+                key={book._id}
+                title={book.title}
+                link={book.link}
+                authors={book.authors.join(", ")}
+                description={book.description}
+                image={book.image}
+                Button={() => (
+                    <a className="btn-floating btn-large waves-effect waves-light red">
+                        onClick={() => this.handleBookDelete(book._id)}<i className="material-icons">delete</i>
+                    </a>
+                )}
+            />
             )
-        }
+        )
+        : 
+        (
+            <React.Fragment>
+                <h2 className="text-center">No Saved Books</h2>
+            </React.Fragment>
+        )
+        
         return (
             <React.Fragment>
                 <Navbar/>
@@ -63,6 +68,7 @@ class Saved extends Component {
                 
             </React.Fragment>
         )
+        
     }
 }
 
